b07ccee80a2e18e2f561f81f2fe06ad6dd78b4aa
Mike Samuel <mikesamuel@gmail.com>
2024-03-19 14:50:34 -0600

    fix bug: recursion in class getter example
    Signed-off-by: Mike Samuel <mikesamuel@gmail.com>

--- examples/class-method.mjs
+++ examples/class-method.mjs
@@ -8,7 +8,7 @@ class C {
   get x() {
     // One can specify inputs to the constructor in which
     // case it synthesizes an insance.
-    return $$0 = this.x;
+    return $$0 = this._x;
     COMEHERE:with ('getter for x', C.this.x = 42) {
       console.log(...$$0);
     }
@@ -17,7 +17,7 @@ class C {
   method(a) {
     // Or one can say use this particular value for `this`.
     COMEHERE:with ('method', C.this = new C(1), a = 2) {
-      console.log('in method', this);
+      console.log(this, '. method(', a, ')');
     }
   }
 
